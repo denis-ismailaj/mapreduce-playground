d8500f1e390e57f197428e8e4da7778dc0416976
Denis Ismailaj <denis.ismailaj1@gmail.com>
2021-11-08 17:51:13 +0100

    Initial structure based on 6.824

---
+++ src/main/loader.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"mapreduce/mr"
+	"plugin"
+)
+
+//
+// load the application Map and Reduce functions
+// from a plugin file, e.g. mapreduce/mrapps/wc.so
+//
+func loadPlugin(filename string) (func(string, string) []mr.KeyValue, func(string, []string) string) {
+	p, err := plugin.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot load plugin %v", filename)
+	}
+	xmapf, err := p.Lookup("Map")
+	if err != nil {
+		log.Fatalf("cannot find Map in %v", filename)
+	}
+	mapf := xmapf.(func(string, string) []mr.KeyValue)
+	xreducef, err := p.Lookup("Reduce")
+	if err != nil {
+		log.Fatalf("cannot find Reduce in %v", filename)
+	}
+	reducef := xreducef.(func(string, []string) string)
+
+	return mapf, reducef
+}
---
+++ src/main/mrworker.go
@@ -0,0 +1,26 @@
+package main
+
+//
+// start a worker process, which is implemented
+// in mapreduce/mr/worker.go. typically there will be
+// multiple worker processes, talking to one coordinator.
+//
+// go run mrworker.go wc.so
+//
+// Please do not change this file.
+//
+
+import "mapreduce/mr"
+import "os"
+import "fmt"
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: mrworker xxx.so\n")
+		os.Exit(1)
+	}
+
+	mapf, reducef := loadPlugin(os.Args[1])
+
+	mr.Worker(mapf, reducef)
+}
